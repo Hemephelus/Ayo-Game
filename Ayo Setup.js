31d48dbb6d848ebde8b3aedc4020c5fb08e5b11b
Hemephelus <nwachukwuujubuonu@gmail.com>
2022-12-16 09:50:26 +0100

    creating render stats feature

--- Ayo Setup.js
+++ Ayo Setup.js
@@ -1,63 +1,139 @@
-class AyoBids {
-    constructor(
-    Pots,
-      x,
-      y,
-      r
-  
-    ) {
-      this.Pots = BidsPerPot.length;
-      this.x = x
-        this.y = y
-        this.r = r
-  
-  
+function selectStartingPosition() {
+  for (let i = 0; i < 2; i++) {
+    let Pots = selectAll(sideDiv[i]);
+
+    for (let j = 0; j < Pots.length; j++) {
+      Pots[j].mousePressed(function () {
+        let startingPosition = [i, j];
+        let startingValue =
+          gameStatesHistory[gameStatesHistory.length - 1]['ayoBoard'][i][j];
+        let stopPosition = startingPosition[1] + startingValue;
+        gameStatesHistory[gameStatesHistory.length - 1]['currentPlayer'] = (selectedPot%2)+1
+        selectedPot++
+        playAyoGameSession(startingPosition, startingValue, stopPosition);
+      });
     }
-  
-  
-   DrawBids(BidsPerPot) {
-    fill(196);
-    textSize(16);
-    for (let i = this.Pots - 1; i >= 0; i--) {
-      if (i < 6) {
+  }
+}
+
+function playAyoGameSession(startingPosition, startingValue, stopPosition){
+  latestGameState = gameStatesHistory[gameStatesHistory.length-1]
+  startingPosition= playAyoGame(startingPosition, stopPosition,latestGameState)
+  latestGameState = gameStatesHistory[gameStatesHistory.length-1]
+  let [side,pot]= startingPosition
+  startingValue = latestGameState['ayoBoard'][side][pot]
+  stopPosition = pot+startingValue
+
+  while(startingValue > 1){
+    latestGameState = gameStatesHistory[gameStatesHistory.length-1]
+    // console.log(latestGameState)
+    startingPosition= playAyoGame(startingPosition, stopPosition,latestGameState)
+    latestGameState = gameStatesHistory[gameStatesHistory.length-1]
+    let [side,pot]= startingPosition
+    startingValue = latestGameState['ayoBoard'][side][pot]
+    stopPosition = pot+startingValue
+  }
+
+}
+
+function playAyoGame(startingPosition, stopPosition,latestGameState) {
+  let tempAyoState = JSON.parse(
+    JSON.stringify(latestGameState)
+    );
+    tempAyoState['ayoBoard'][startingPosition[0]][startingPosition[1]] = 0;
+    gameStatesHistory.push(tempAyoState);
+    let currentPot, side;
+    for (let i = startingPosition[1]; i < stopPosition; i++) {
+      latestGameState = gameStatesHistory[gameStatesHistory.length-1]
+      tempAyoState = JSON.parse(
+        JSON.stringify(latestGameState)
+        );
+        side = (startingPosition[0] + (floor((i + 1) / 6) % 2)) % 2;
+        currentPot = (i + 1) % 6;
+        tempAyoState['ayoBoard'][side][currentPot] += 1;
         
-        text(BidsPerPot[i], i * 70 + 115, 320);
-        // console.log("      "+BidsPerPot[0])
-        for (let j = 0; j < BidsPerPot[i]; j++) {
-          // console.log("      "+BidsPerPot[i])
-          circle(this.x, this.y, 10);
-        }
-      } else {
-        for (let j = 0; j < BidsPerPot[i]; j++) {
-          circle(this.x, this.y, 10);
-        }
-        text(BidsPerPot[i], 70 * (6 - i) + 465, 130);
-      }
+        gameStatesHistory.push(tempAyoState);
+        if(tempAyoState['ayoBoard'][side][currentPot] === 4){
+          latestGameState = gameStatesHistory[gameStatesHistory.length-1]
+          tempAyoState = JSON.parse(
+            JSON.stringify(latestGameState)
+            );
+            tempAyoState['ayoBoard'][side][currentPot] = 0;
+            tempAyoState['playerScore'+side] += 4
+            gameStatesHistory.push(tempAyoState);
+          }
+          
+
+
+  }
+
+  return [side, currentPot]
+}
+
+
+function Control_Buttons() {
+  // Pause Button
+  select("#pause").mousePressed(function () {
+    pauseGame = true;
+    if (pauseGame) {
+      playAyoGame = false;
+      fastForwardGame = false;
+      frameR = 1;
+      noLoop();
     }
-  }
-  
-    
-  DrawBoard() {
-    fill(198, 100, 0);
-    rect(75, 150, 450, 70, 90);
-    rect(75, height/2, 450, 70, 90);
-  }
-    
-  
-    DrawPots() {
-    fill(158, 80, 0);
-    for (let i = 0; i < this.Pots; i++) {
-      if (i < 6) {
-       
-        circle(70 * i + 125, 185, 55);
-       
-      } else { if(i == 7){
-          fill(100,0,0)
-        }
-        circle(70 * (i - 6) + 125, 255, 55);
-               fill(158, 80, 0);
-      }
+  });
+
+  select("#reset").mousePressed(function () {
+    resetGame = true;
+    if (resetGame) {
+      pauseGame = true;
+
+      // playGame = false;
+      a = 0;
+      selectedPot = 0;
+      loop();
     }
-  }
-    
-  }  
+  });
+
+  // Play Button
+  select("#play_arrow").mousePressed(function () {
+    playGame = true;
+    if (playGame) {
+      pauseGame = false;
+      fastForwardGame = true;
+      frameR = 1;
+      loop();
+    }
+  });
+
+  // Fast Forward Button
+  select("#fast_forward").mousePressed(function () {
+    fastForwardGame = true;
+    if (playGame) {
+      frameR = 10;
+      loop();
+    }
+  });
+
+  // Fast Forward Button
+  select("#skip_next").mousePressed(function () {
+    skipNextGame = true;
+    if (skipNextGame) {
+      playGame = false;
+      a++;
+      loop();
+    }
+    noLoop();
+  });
+
+  // Skip Previous Button
+  select("#skip_previous").mousePressed(function () {
+    skipPreviousGame = true;
+    if (skipPreviousGame) {
+      playGame = false;
+      a--;
+      loop();
+    }
+    noLoop();
+  });
+}
--- sketch.js
+++ sketch.js
@@ -1,209 +1,167 @@
-let BidsPerPot = [4, 4];
-let BoardInfo = [];
-let StartingPosition = 2;
-let StartingValue = BidsPerPot[StartingPosition];
-let RestPot = StartingPosition + StartingValue;
-let Player1Pt = 0;
-let Player2Pt = 0;
-let i = 0;
-let Ayo;
+// let StartingPosition = 2;
+// let StartingValue = BidsPerPot[StartingPosition];
+// let RestPot = StartingPosition + StartingValue;
+// let Player1Pt = 0;
+// let Player2Pt = 0;
+// let i = 0;
+// let Ayo;
 let sideDiv = [".Side.one > .pot", ".Side.two > .pot"];
 frameR = 1;
-let ayoBoard = [
-  [4, 4, 4, 4, 4, 4],
-  [4, 4, 4, 4, 4, 4],
-];
-let a= [0,0]
-let gameStatesHistory = [ayoBoard]
+let pauseGame = false;
+let playGame = true;
+let resetGame = false;
+let W2N = {
+  '1': 'one',
+  '2': 'two',
+}
+let gameState = {
+  'ayoBoard': [
+    [4, 4, 4, 4, 4, 4],
+    [4, 4, 4, 4, 4, 4],
+  ],
+  'playerScore0': 0,
+  'playerScore1': 0,
+  'currentPlayer' : 1,
+  'startingPosition' : 1,
+};
+let gameStatesHistory = [gameState];
 
 function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
   cnv.position(0, 0);
   cnv.style("z-index", -1);
-  MakeObject(BidsPerPot);
-  selectStartingPosition()
+  // MakeObject(BidsPerPot);
+  selectStartingPosition();
+  Control_Buttons();
 }
 
-function selectStartingPosition() {
-  for (let i = 0; i < 2; i++) {
-    let Pots = selectAll(sideDiv[i]);
-
-    for (let j = 0; j < Pots.length; j++) {
-      Pots[j].mousePressed(function () {
-       let startingPosition = [i, j] ;
-       let startingValue = ayoBoard[i][j]
-       playGame(startingPosition,startingValue)
-       b++
-      });
-    }
-  }
-}
-
-function playGame(startingPosition,startingValue){
-  for(let i = 0; i < startingValue; i++ ){
-    tempAyoBoard = JSON.parse(JSON.stringify(gameStatesHistory[gameStatesHistory.length-1]));
-    tempAyoBoard[0][i+1] += 1
-    gameStatesHistory.push(tempAyoBoard)
-  }
-}
-a = 0
-b = 0
+let a = 0;
+let selectedPot = 0;
 
 function draw() {
   //MakeObject(BidsPerPot)
   frameRate(frameR);
-  // background(28);
-  
-  console.log(gameStatesHistory[a])
-  for (let i = 0; i < gameStatesHistory[a].length; i++) {
+  let currentBoardState = gameStatesHistory[a]['ayoBoard']
+  for (let i = 0; i < currentBoardState.length; i++) {
     let btns = selectAll(sideDiv[i]);
-    for (let j = 0; j < gameStatesHistory[a][i].length; j++) {
-      btns[j].elt.innerText = gameStatesHistory[a][i][j];
+    for (let j = 0; j < currentBoardState[i].length; j++) {
+      btns[j].elt.innerText = currentBoardState[i][j];
     }
   }
-
-  if(b > 0 && b < 4){
-    a++
-  }else{
-    noLoop
+  let currentPlayerNum = gameStatesHistory[a]['currentPlayer']
+  select(".game_state>span").elt.innerText = a;
+  select(".current_player>span").elt.innerText = currentPlayerNum;
+  select(`.player.${W2N[currentPlayerNum]}`).addClass('current_style')
+  select(`.player.${W2N[(currentPlayerNum%2)+1]}`).removeClass('current_style')
+  select('.player.one .score >span').elt.innerText = gameStatesHistory[a]['playerScore0']
+  select('.player.two .score >span').elt.innerText = gameStatesHistory[a]['playerScore1']
+  
+  if (selectedPot > 0 && a < gameStatesHistory.length - 1 && playGame === true) {
+    a++;
+  } else {
+    noLoop();
   }
- 
-  // frameR++
 }
 
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
 // function getall
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function MakeObject(BidsPerPot) {
-  fill(196);
-  textSize(16);
-  for (let i = BidsPerPot.length - 1; i >= 0; i--) {
-    if (i < 6)
-      for (let j = 0; j < BidsPerPot[i]; j++) {
-        let x = 70 * i + 100 + random(15, 35);
-        let y = 225 + random(15, 35);
-        let Pots = BidsPerPot.length;
-        let r = random(10, 11);
-        Ayo = new AyoBids(Pots, x, y, r);
-        BoardInfo.push(Ayo);
-      }
-    else {
-      for (let j = 0; j < BidsPerPot[i]; j++) {
-        let x = 70 * (6 - i) + 445 + random(15, 35);
-        let y = 155 + random(15, 35);
-        let Pots = BidsPerPot.length;
-        let r = random(10, 11);
-
-        Ayo = new AyoBids(Pots, x, y, r);
-        BoardInfo.push(Ayo);
-      }
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function MoveBids(BidsPerPot, StartingPosition, StartingValue, RestPot) {
-  if (StartingValue != 0) {
-    for (
-      StartingPosition = StartingPosition;
-      StartingPosition < RestPot;
-      StartingPosition++
-    ) {
-      BoardInfo.pop();
-      BidsPerPot[(StartingPosition + 1) % 12] =
-        BidsPerPot[(StartingPosition + 1) % 12] + 1;
-
-      if (BidsPerPot[(StartingPosition + 1) % 12] == 4) {
-        if ((StartingPosition + 1) % 12 < 6) {
-          Player1Pt = Player1Pt + 4; //Add 4 to player 1
-        } else {
-          Player2Pt = Player2Pt + 4; //Add 4 to player 2
-        }
-
-        BidsPerPot[(StartingPosition + 1) % 12] = 0;
-      }
-    }
-
-    //     if (BidsPerPot[(StartingPosition + 0) % 12] == 0) {
-
-    //       if (Path[DepthLevel] < 6 && (StartingPosition % 12) > 5) {//Player1
-
-    //         Player1Pt = Player1Pt + 4
-    //         Player2Pt = Player2Pt - 4 //Add 4 to player 1
-
-    //       } else if (Path[DepthLevel] > 5 && (StartingPosition % 12) < 6) {//Player2
-
-    //         Player1Pt = Player1Pt - 4
-    //         Player2Pt = Player2Pt + 4
-
-    //       }
-    //     }
-
-    let ZeroPosition = StartingPosition - StartingValue;
-    BidsPerPot[ZeroPosition % 12] = 0;
-    StartingValue = BidsPerPot[StartingPosition % 12];
-    RestPot = StartingPosition + StartingValue;
-
-    if (StartingValue > 1) {
-      MoveBids(BidsPerPot, StartingPosition, StartingValue, RestPot);
-    }
-
-    return BidsPerPot;
-  }
-
-  return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-}
+// function MakeObject(BidsPerPot) {
+//   fill(196);
+//   textSize(16);
+//   for (let i = BidsPerPot.length - 1; i >= 0; i--) {
+//     if (i < 6)
+//       for (let j = 0; j < BidsPerPot[i]; j++) {
+//         let x = 70 * i + 100 + random(15, 35);
+//         let y = 225 + random(15, 35);
+//         let Pots = BidsPerPot.length;
+//         let r = random(10, 11);
+//         Ayo = new AyoBids(Pots, x, y, r);
+//         BoardInfo.push(Ayo);
+//       }
+//     else {
+//       for (let j = 0; j < BidsPerPot[i]; j++) {
+//         let x = 70 * (6 - i) + 445 + random(15, 35);
+//         let y = 155 + random(15, 35);
+//         let Pots = BidsPerPot.length;
+//         let r = random(10, 11);
+
+//         Ayo = new AyoBids(Pots, x, y, r);
+//         BoardInfo.push(Ayo);
+//       }
+//     }
+//   }
+// }
+
+// function MoveBids(BidsPerPot, StartingPosition, StartingValue, RestPot) {
+//   if (StartingValue != 0) {
+//     for (
+//       StartingPosition = StartingPosition;
+//       StartingPosition < RestPot;
+//       StartingPosition++
+//     ) {
+//       BoardInfo.pop();
+//       BidsPerPot[(StartingPosition + 1) % 12] =
+//         BidsPerPot[(StartingPosition + 1) % 12] + 1;
+
+//       if (BidsPerPot[(StartingPosition + 1) % 12] == 4) {
+//         if ((StartingPosition + 1) % 12 < 6) {
+//           Player1Pt = Player1Pt + 4; //Add 4 to player 1
+//         } else {
+//           Player2Pt = Player2Pt + 4; //Add 4 to player 2
+//         }
+
+//         BidsPerPot[(StartingPosition + 1) % 12] = 0;
+//       }
+//     }
+
+//     //     if (BidsPerPot[(StartingPosition + 0) % 12] == 0) {
+
+//     //       if (Path[DepthLevel] < 6 && (StartingPosition % 12) > 5) {//Player1
+
+//     //         Player1Pt = Player1Pt + 4
+//     //         Player2Pt = Player2Pt - 4 //Add 4 to player 1
+
+//     //       } else if (Path[DepthLevel] > 5 && (StartingPosition % 12) < 6) {//Player2
+
+//     //         Player1Pt = Player1Pt - 4
+//     //         Player2Pt = Player2Pt + 4
+
+//     //       }
+//     //     }
+
+//     let ZeroPosition = StartingPosition - StartingValue;
+//     BidsPerPot[ZeroPosition % 12] = 0;
+//     StartingValue = BidsPerPot[StartingPosition % 12];
+//     RestPot = StartingPosition + StartingValue;
+
+//     if (StartingValue > 1) {
+//       MoveBids(BidsPerPot, StartingPosition, StartingValue, RestPot);
+//     }
+
+//     return BidsPerPot;
+//   }
+
+//   return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// }
